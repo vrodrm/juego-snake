6dae5d1bb64ed3d87a30776719f94fcb5b053b39
Vicente Rodriguez <vrodriguezm122@gmail.com>
2025-04-13 16:03:21 +0200

    add snake and stars

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const tablero = [];
 let direccion = DERECHA;
 let nuevaDireccion = DERECHA;
 let longitudSerpiente;
+let tiempo;
 
 function inicio() {
         // Llenamos el tablero de ceros (VACIO)
@@ -20,31 +21,33 @@ function inicio() {
                 }
         }
 
-        // Colocamos la serpiente
-        // El 1 es la cabeza de la serpiente
-        tablero[2][7] = 3;
-        tablero[3][7] = 2;
-        tablero[4][7] = 1;
-        longitudSerpiente = 3;
-
-        console.log(tablero);
-
         //Añadimos event listener para manejar cuando el usuario pulsa las teclas
         onkeydown = (KeyboardEvent) => {
                 cambiarDireccion(KeyboardEvent.code);
         }
 
         colorearTablero();
-        mostrarTablero();
+
+        // Colocamos la serpiente
+        // El 1 es la cabeza de la serpiente
+        tablero[2][7] = 3;
+        tablero[3][7] = 2;
+        tablero[4][7] = 1;
+        document.getElementById('img2-7').src = '../assets/img/cola.png';
+        document.getElementById('img3-7').src = '../assets/img/cuerpo.png';
+        document.getElementById('img4-7').src = '../assets/img/cabeza.png';
+        longitudSerpiente = 3;
+
+        colocarEstrella();
 
         setTimeout(() => {
                 actualizarTablero();
-        }, 2000);
+        }, 300);
 }
 
 function actualizarTablero() {
         let xCabeza, yCabeza;
-        setInterval(() => {
+        tiempo = setInterval(() => {
                 //Buscamos la cabeza de la serpiente
                 for (let i = 0; i < 17; i++) {
                         for (let j = 1; j < 15; j++) {
@@ -60,6 +63,17 @@ function actualizarTablero() {
                         for (let j = 0; j < 15; j++) {
                                 if (tablero[i][j] === longitudSerpiente) {
                                         tablero[i][j] = VACIO;
+                                        document.getElementById('img' + i + '-' + j).src = '';
+                                        break;
+                                }
+                        }
+                }
+                
+                // Buscamos la nueva cola y la pintamos
+                for (let i = 0; i < 17; i++) {
+                        for (let j = 0; j < 15; j++) {
+                                if (tablero[i][j] === longitudSerpiente-1) {
+                                        document.getElementById('img' + i + '-' + j).src = '../assets/img/cola.png';
                                         break;
                                 }
                         }
@@ -79,39 +93,55 @@ function actualizarTablero() {
                         case ARRIBA:
                                 if (direccion != ABAJO) {
                                         tablero[xCabeza][yCabeza - 1] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + xCabeza + '-' + (yCabeza - 1)).src = '../assets/img/cabeza.png';
                                         direccion = ARRIBA;
                                 } else  {
                                         tablero[xCabeza][yCabeza + 1] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + xCabeza + '-' + (yCabeza + 1)).src = '../assets/img/cabeza.png';
                                 }
                                 break;
                         case ABAJO:
                                 if (direccion != ARRIBA) {
                                         tablero[xCabeza][yCabeza + 1] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + xCabeza + '-' + (yCabeza + 1)).src = '../assets/img/cabeza.png';
                                         direccion = ABAJO;
                                 } else {
                                         tablero[xCabeza][yCabeza - 1] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + xCabeza + '-' + (yCabeza - 1)).src = '../assets/img/cabeza.png';
                                 }
                                 break;
                         case DERECHA:
                                 if(direccion != IZQUIERDA) {
                                         tablero[xCabeza + 1][yCabeza] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + (xCabeza + 1) + '-' + yCabeza).src = '../assets/img/cabeza.png';
                                         direccion = DERECHA;
                                 } else {
                                         tablero[xCabeza - 1][yCabeza] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + (xCabeza - 1) + '-' + yCabeza).src = '../assets/img/cabeza.png';
                                 }
                                 break;
                         case IZQUIERDA:
                                 if(direccion != DERECHA) {
                                         tablero[xCabeza - 1][yCabeza] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + (xCabeza - 1) + '-' + yCabeza).src = '../assets/img/cabeza.png';
                                         direccion = IZQUIERDA;
                                 } else {
                                         tablero[xCabeza + 1][yCabeza] = 1;
+                                        document.getElementById('img' + xCabeza + '-' + yCabeza).src = '../assets/img/cuerpo.png';
+                                        document.getElementById('img' + (xCabeza + 1) + '-' + yCabeza).src = '../assets/img/cabeza.png';
                                 }
                                 break;
                 }
 
                 mostrarTablero();
-        }, 1000);
+        }, 100);
 }
 
 function cambiarDireccion(codigoTecla) {
@@ -123,32 +153,42 @@ function cambiarDireccion(codigoTecla) {
                 nuevaDireccion = DERECHA;
         } else if (codigoTecla === 'ArrowLeft' || codigoTecla === 'KeyA') {
                 nuevaDireccion = IZQUIERDA;
+        } else if (codigoTecla === 'Space') {
+                clearInterval(tiempo);  
+                console.log(tiempo);
         }
 }
 
-function colorearTablero(){
-        let color = true;
-        document.querySelectorAll('.casilla').forEach((casilla) => {
-                if(color){
-                        casilla.style.backgroundColor = '#141D73';
-                } else {
-                        casilla.style.backgroundColor = '#464180';
-                }
-                color = !color;
-        });
+// Función para colorear el tablero y asignar imagenes a las casillas
+function colorearTablero() {
+    let color = true;
+    let x = 0, y = 0;
+    let imagen;
+    document.querySelectorAll('.casilla').forEach((casilla) => {
+        if (color) {
+            casilla.style.backgroundColor = '#141D73';
+        } else {
+            casilla.style.backgroundColor = '#464180';
+        }
+        imagen = document.createElement('img');
+        imagen.id = 'img' + x + '-' + y; 
+        casilla.appendChild(imagen);
+        x++;
+        if (x == 17) {
+            x = 0;
+            y++;
+        }
+        color = !color;
+    });
 }
 
-function mostrarTablero() {
-        /*
-        let htmlTablero = '<table>';
-        for (let i = 0; i < 15; i++) {
-                htmlTablero += '<tr>';
-                for (let j = 0; j < 17; j++) {
-                        htmlTablero += '<td>' + tablero[j][i] + '</td>';
-                }
-                htmlTablero += '</tr>';
+function colocarEstrella(){
+        let x = Math.floor(Math.random() * 17);
+        let y = Math.floor(Math.random() * 15);
+        while(tablero[x][y] != VACIO){
+            x = Math.floor(Math.random() * 17);
+            y = Math.floor(Math.random() * 15);
         }
-        htmlTablero += '</table>';
-        document.getElementById('tablero').innerHTML = htmlTablero;
-        */
+        tablero[x][y] = -1;
+        document.getElementById('img' + x + '-' + y).src = '../assets/img/estrella.svg';
 }
